e0351c552bba788e7e01e5fe3c3866f1e0fb8ef6
a71evorgyan <arminegevorgyan1993@gmail.com>
2022-11-26 15:07:02 +0400

    fix(): remove unnecessary check

--- src/common/exceptions/messages.ts
+++ src/common/exceptions/messages.ts
@@ -2,5 +2,3 @@ export const USER_NOT_FOUND = 'User not found';
 export const INCORRECT_CREDENTIALS = 'Username or password is incorrect';
 export const USER_ALREADY_EXISTS = 'User is already exists';
 export const INVALID_REQUEST_BODY = 'Invalid Request Body';
-export const INVALID_PASSWORD_LENGTH =
-  'Password should be at least 8 characters long';
--- src/modules/auth/strategies/local.strategy.ts
+++ src/modules/auth/strategies/local.strategy.ts
@@ -5,10 +5,7 @@ import {
 } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
-import {
-  INVALID_PASSWORD_LENGTH,
-  INVALID_REQUEST_BODY,
-} from '../../../common/exceptions/messages';
+import { INCORRECT_CREDENTIALS } from '../../../common/exceptions/messages';
 import { AuthService } from '../auth.service';
 
 @Injectable()
@@ -19,11 +16,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
 
   async validate(username: string, password: string) {
     if (!username || !password)
-      throw new BadRequestException(INVALID_REQUEST_BODY);
-
-    if (password.length < 8) {
-      throw new BadRequestException(INVALID_PASSWORD_LENGTH);
-    }
+      throw new BadRequestException(INCORRECT_CREDENTIALS);
 
     const user = await this.authService.validateUser(username, password);
     if (!user) {
